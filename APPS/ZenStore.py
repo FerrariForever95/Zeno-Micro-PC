68cd8224c1b96218ec5dd81e444b13a5a52529d3
M.Venkata Shanmukha Viswanadh <shanmukhamarthi@gmail.com>
2025-12-10 14:53:11 +0530

    Update APPS/ZenStore.py from Zeno UI Editor

--- APPS/ZenStore.py
+++ APPS/ZenStore.py
@@ -16,23 +16,25 @@ def main(ui=None, on_exit=None):
         ui.begin()
 
     # Create screen
-    screen = UIScreen(ui, background=color565(255, 255, 255), taskbarcolor=color565(131, 141, 237), taskbar_text='ZenStore', taskbar_text_color=color565(255, 255, 255), on_exit=on_exit)
+    screen = UIScreen(ui, background=color565(255, 255, 255), taskbarcolor=color565(128, 0, 64), taskbar_text='ZenStore', taskbar_text_color=color565(255, 255, 255), on_exit=on_exit)
 
     screen.start(ui)
 
     # Draw layers (colored rectangles)
-    screen.layer(2, 37, 315, 10, color565(80, 80, 80))
-    screen.layer(2, 49, 316, 38, color565(192, 192, 192))
-    screen.layer(2, 88, 316, 149, color565(235, 235, 235))
+    screen.layer(1, 37, 317, 17, color565(80, 80, 80))
+    screen.layer(1, 55, 318, 37, color565(192, 192, 192))
+    screen.layer(1, 93, 318, 146, color565(229, 229, 229))
+    screen.layer(3, 95, 313, 70, color565(190, 190, 190))
+    screen.layer(3, 166, 314, 70, color565(193, 193, 193))
 
     # Create and draw buttons
     buttons = []
     def on_button_click():
         print('Button Install pressed')
 
-    buttons.append(UIButton(5, 53, 84, 30, label='Install', color=color565(223, 223, 223), text_color=color565(0, 0, 0), margin=5, action=on_button_click))
-    buttons.append(UIButton(91, 53, 84, 30, label='Uninstall', color=color565(223, 223, 223), text_color=color565(0, 0, 0), margin=5, action=on_button_click))
-    buttons.append(UIButton(177, 53, 137, 30, label='Search', color=color565(223, 223, 223), text_color=color565(0, 0, 0), margin=5, action=on_button_click))
+    buttons.append(UIButton(3, 58, 100, 30, label='Install', color=color565(224, 224, 224), text_color=color565(0, 0, 0), margin=5, action=on_button_click))
+    buttons.append(UIButton(107, 58, 100, 30, label='Uninstall', color=color565(223, 223, 223), text_color=color565(0, 0, 0), margin=5, action=on_button_click))
+    buttons.append(UIButton(211, 58, 105, 30, label='Search', color=color565(223, 223, 223), text_color=color565(0, 0, 0), margin=5, action=on_button_click))
 
     for btn in buttons:
         btn.draw(ui)
