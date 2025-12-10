8d0ceabb3f1bf4b43c9b7c74f83f602316969bdf
M.Venkata Shanmukha Viswanadh <shanmukhamarthi@gmail.com>
2025-12-10 12:57:17 +0530

    Update APPS/ZenStore.py from Zeno UI Editor

--- APPS/ZenStore.py
+++ APPS/ZenStore.py
@@ -1,13 +1,13 @@
 # Auto-generated Zeno UI screen
-# App: ZenStore (v2.3) by Phoniex
+# App: ZenStore (v1.0.0) by Phoenix
 
 import time
 from ili9341 import color565
 from firmware import HUIModule, UIScreen
 
 APP_NAME    = 'ZenStore'
-APP_AUTHOR  = 'Phoniex'
-APP_VERSION = '2.3'
+APP_AUTHOR  = 'Phoenix'
+APP_VERSION = '1.0.0'
 
 def main(ui=None, on_exit=None):
     # Create UI context if not provided
@@ -16,16 +16,16 @@ def main(ui=None, on_exit=None):
         ui.begin()
 
     # Create screen
-    screen = UIScreen(ui, background=color565(255, 255, 255), taskbarcolor=color565(0, 0, 64), taskbar_text='ZenStore', taskbar_text_color=color565(255, 255, 255), on_exit=on_exit)
+    screen = UIScreen(ui, background=color565(255, 255, 255), taskbarcolor=color565(64, 0, 128), taskbar_text='ZenStore', taskbar_text_color=color565(255, 255, 255), on_exit=on_exit)
 
     screen.start(ui)
 
     # Draw layers (colored rectangles)
-    screen.layer(5, 41, 310, 44, color565(203, 205, 211))
-    screen.layer(5, 87, 309, 146, color565(80, 80, 80))
+    screen.layer(2, 39, 316, 25, color565(225, 225, 225))
+    screen.layer(2, 66, 316, 171, color565(202, 202, 202))
+    screen.layer(5, 101, 310, 133, color565(216, 216, 216))
 
     # Fade in / backlight safety
-    
     ui.fade_in(fade_time=0.4)
 
 
