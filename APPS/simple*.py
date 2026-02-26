f11927068bfe65622e14d85c784be86b326f5f55
M.Venkata Shanmukha Viswanadh <shanmukhamarthi@gmail.com>
2026-02-26 22:54:21 +0530

    Update APPS/simple*.py from Zeno UI Studio Editor

---
+++ APPS/simple*.py
@@ -0,0 +1,65 @@
+# Auto-generated Zeno UI Studio screen
+# App: simple* (v1.0.0) by User
+
+import time
+from Graphics import color565 ,UIScreen, UIButton, UIText, UISlider, UIToggleSwitch, UIProgressBar, UIPanel
+import zeno
+
+APP_NAME    = 'simple*'
+APP_AUTHOR  = 'User'
+APP_VERSION = '1.0.0'
+
+ui = zeno.ui
+
+def on_exit():
+    raise APP_EXIT
+
+def main():
+
+    screen = UIScreen(ui, background=color565(0, 0, 0), taskbarcolor=color565(50, 50, 50), taskbar_text='Untitled1', taskbar_text_color=color565(255, 255, 255), on_exit=on_exit)
+
+    screen.start_withoutexit(ui)
+
+
+
+    buttons = []
+    sliders = []
+    toggles = []
+    progress_bars = []
+    panels = []
+
+    toggles.append(UIToggleSwitch(238, 59, 50, state=True))
+
+    for panel in panels:
+        panel.draw(ui)
+
+    for bar in progress_bars:
+        bar.draw(ui)
+
+    for slider in sliders:
+        slider.draw(ui)
+
+    for toggle in toggles:
+        toggle.draw(ui)
+
+    for btn in buttons:
+        btn.draw(ui)
+
+    ui.fade_in(fade_time=0.4)
+
+    while True:
+        screen.check(ui)
+        for btn in buttons:
+            btn.get_touch(ui)
+
+        for slider in sliders:
+            slider.handle_touch(ui)
+
+        for toggle in toggles:
+            toggle.handle_touch(ui)
+
+
+        time.sleep(0.05)
+
+
+main()
